65fef6870901f8a3d74c36a809444c44ef306ab6
fariha zahra <fariha.qorinaa@gmail.com>
2025-03-12 23:15:48 +0900

    step 9-3: modified App.css and ItemList.txt for better visual

--- typescript/simple-mercari-web/src/components/ItemList.tsx
+++ typescript/simple-mercari-web/src/components/ItemList.tsx
@@ -30,12 +30,14 @@ export const ItemList = ({ reload, onLoadCompleted }: Prop) => {
   }, [reload, onLoadCompleted]);
 
   return (
-    <div>
+    <div className='ItemList'>
       {items?.map((item) => {
         return (
           <div key={item.id} className="ItemList">
             {/* TODO: Task 2: Show item images */}
-            <img src={BASE_URL+item.image_name} />
+            {/* basically localhost:3000/image/image_name */}
+
+            <img className Image src={BASE_URL+item.image_name} />
             <p>
               <span>Name: {item.name}</span>
               <br />
